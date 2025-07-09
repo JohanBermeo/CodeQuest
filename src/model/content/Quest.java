1a859761acb2b001bdd705b6c7e261a646caa186
Crissmann23 <cfmancoh@udistrital.edu.co>
2025-07-08 19:42:51 -0500

    Quest.java

---
+++ src/model/content/Quest.java
@@ -0,0 +1,14 @@
+package model.content;
+
+public class Quest {
+    private String id;
+    private String title;
+    private String description;
+    private String author;
+
+    public Quest(String id, String title, String description, String author) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }}
